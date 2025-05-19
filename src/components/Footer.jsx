ed019d1f38d19ca1edf8ab3ed8031c7a40429677
shivam maurya <shivammaurya3659@outlook.com>
2025-05-20 00:25:58 +0530

    footer icon chagnes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import {
   FaFacebook,
-  FaTwitter,
   FaInstagram,
   FaLinkedinIn,
+  FaGithub,
 } from "react-icons/fa6";
 function Footer() {
   return (
@@ -12,12 +12,24 @@ function Footer() {
       <footer className="py-12">
         <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
           <div className=" flex flex-col items-center justify-center">
-            <div className="flex space-x-4">
-              <FaFacebook size={24} />
-              <FaTwitter size={24} />
-              <FaInstagram size={24} />
-              <FaLinkedinIn size={24} />
-            </div>
+            <ul className='flex space-x-4'>
+                                                <li>
+                                                    <a href="https://www.facebook.com/share/15z3FQichX/" target="_blank">
+                                                        <FaFacebook className="text-2xl cursor-pointer hover:scale-110 duration-200" /> </a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://www.linkedin.com/in/siva-kant-maurya-5789b9287" target="_blank">
+                                                        <FaLinkedinIn className="text-2xl cursor-pointer hover:scale-110 duration-200" /> </a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://www.instagram.com/shivam_maurya7244?igsh=b2twYTByZzIxa3cy" target="_blank">
+                                                        <FaInstagram className="text-2xl cursor-pointer hover:scale-110 duration-200" /> </a>
+                                                </li>
+                                                <li>
+                                                    <a href="https://github.com/sivamaurya7244" target="_blank">
+                                                        <FaGithub className="text-2xl cursor-pointer hover:scale-110 duration-200" /> </a>
+                                                </li>
+                                            </ul>
             <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
               <p className="text-sm">
                 &copy; 2025 My Portfolio. All rights reserved.
